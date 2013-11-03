a29ef63ea757132ec8d5ee1b899a92f58d461f89
unknown <mark prades@markprades.(none)>
2013-11-03 11:06:50 +0100

    commit

---
+++ test/utils-test.js
@@ -0,0 +1,10 @@
+var utils;
+
+utils = require(path.join(__dirname, "/../lib/utils.js"));
+
+describe("utils", function() {
+	const IP = "83.112.245.11";
+	it("should find the country from an ip", function(done) {
+		assert.equal(utils.countryFromIp(IP), IP);
+	});
+});

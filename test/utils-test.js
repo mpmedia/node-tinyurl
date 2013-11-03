dd49c2686e7b6ce9974f2fc9a29754be03af8038
unknown <mark prades@markprades.(none)>
2013-11-03 11:08:37 +0100

    update

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,7 +1,6 @@
-var utils;
-
+var utils, path;
+path = require('path');
 utils = require(path.join(__dirname, "/../lib/utils.js"));
-
 describe("utils", function() {
 	const IP = "83.112.245.11";
 	it("should find the country from an ip", function(done) {
